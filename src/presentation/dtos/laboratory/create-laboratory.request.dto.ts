c4ad7f7c30fe94357c742ce587f4948ac3d5612f
Julio Brazao <julio@brazao.dev>
2025-06-09 19:58:36 -0400

    Example to be used in auto-doc

--- src/presentation/dtos/laboratory/create-laboratory.request.dto.ts
+++ src/presentation/dtos/laboratory/create-laboratory.request.dto.ts
@@ -1,15 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsInt, IsString } from 'class-validator';
 
 export class CreateLaboratoryRequestDTO {
+  @ApiProperty({
+    description: 'Name for laboratory',
+    example: 'Example Laborat',
+  })
   @IsString({ message: 'Name cannot be empty!' })
   name: string;
 
+  @ApiProperty({
+    description: 'Location for laboratory',
+    example: 'Main Pavillion',
+  })
   @IsString({ message: 'Location cannot be empty!' })
   location: string;
 
+  @ApiProperty({
+    description: 'Capacity number for laboratory',
+    example: 20,
+  })
   @IsInt({ message: 'You must set capacity for lab.' })
   capacity: number;
 
+  @ApiProperty({
+    description: 'Boolean to define if laboratory is available or not.',
+    example: false,
+  })
   @IsBoolean({ message: 'You must inform if it is available.' })
   isAvailable: boolean;
 }
