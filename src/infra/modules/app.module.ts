9f2a4cbdb54052427c4914f372df46c2b92855c4
Gabriel <gabriel147rocha@gmail.com>
2025-06-09 22:03:27 -0300

    feat : create module reservation

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongoModule } from './mongo.module';
+import { ReservationModule } from './reservation.module';
 
 @Module({
-  imports: [MongoModule],
+  imports: [
+    MongoModule,
+    ReservationModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
