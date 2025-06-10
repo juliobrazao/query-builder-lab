006773ece86228fc083c662b920afda2e7dd8fa1
Gabriel <gabriel147rocha@gmail.com>
2025-06-09 22:10:58 -0300

    module reservation

--- src/presentation/dtos/reservation/reservation-request.dto.ts
+++ src/presentation/dtos/reservation/reservation-request.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class ReservationRequestDTO {
+    @ApiProperty({
+        description: 'laboratoryId for laboratory',
+        example: 'Example 098213472189',
+    })
     @IsString({ message: 'laboratoryId cannot be empty!' })
     laboratoryId: string;
 
+    @ApiProperty({
+        description: 'UserId for laboratory',
+        example: 'Example 792814369217836',
+    })
     @IsString({ message: 'userId cannot be empty!' })
     userId: string;
 }
