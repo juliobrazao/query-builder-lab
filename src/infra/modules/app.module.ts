902f376e41da23dc0b561d9d416d451b05c570db
Gabriel <gabriel147rocha@gmail.com>
2025-06-12 19:40:19 -0300

    fix : imports

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongoModule } from './mongo.module';
-import { ReservationModule } from './reservation.module';
 
 @Module({
   imports: [
-    MongoModule,
-    ReservationModule
+    MongoModule
   ],
 })
 export class AppModule { }
